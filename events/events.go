fca87cf4bd2da7e76ab9f5817e5fe9fc0d5c9d2a
kayos <kayos@tcp.direct>
2025-04-17 15:16:09 -0700

    Fix[flow]: Fix flow and ethernet participants

--- events/events.go
+++ events/events.go
@@ -28,20 +28,43 @@ func (f FileInfo) Empty() bool {
 	return f.Filename == "" && f.Magic == "" && f.State == "" && f.Md5 == "" && f.Stored == false && f.Size == 0
 }
 
+type Bypassed struct {
+	PktsToserver  int `json:"pkts_toserver"`
+	PktsToclient  int `json:"pkts_toclient"`
+	BytesToserver int `json:"bytes_toserver"`
+	BytesToclient int `json:"bytes_toclient"`
+}
+
+type ExceptionPolicy struct {
+	Target string `json:"target"`
+	Policy string `json:"policy"`
+}
+
+type Ether struct {
+	DestMacs []string `json:"dest_macs"`
+	SrcMacs  []string `json:"src_macs"`
+}
+
 type Flow struct {
-	PktsToserver  int    `json:"pkts_toserver,omitempty"`
-	PktsToclient  int    `json:"pkts_toclient,omitempty"`
-	BytesToserver int    `json:"bytes_toserver,omitempty"`
-	BytesToclient int    `json:"bytes_toclient,omitempty"`
-	Start         *Time  `json:"start"`
-	End           *Time  `json:"end"`
-	Age           int    `json:"age,omitempty"`
-	State         string `json:"state"`
-	Reason        string `json:"reason,omitempty"`
+	PktsToserver    int               `json:"pkts_toserver,omitempty"`
+	PktsToclient    int               `json:"pkts_toclient,omitempty"`
+	BytesToserver   int               `json:"bytes_toserver,omitempty"`
+	BytesToclient   int               `json:"bytes_toclient,omitempty"`
+	Bypassed        Bypassed          `json:"bypassed,omitempty"`
+	Start           string            `json:"start,omitempty"`
+	End             string            `json:"end,omitempty"`
+	Age             int               `json:"age,omitempty"`
+	Bypass          string            `json:"bypass,omitempty"`
+	State           string            `json:"state,omitempty"`
+	Reason          string            `json:"reason,omitempty"`
+	Alerted         bool              `json:"alerted,omitempty"`
+	Action          string            `json:"action,omitempty"`
+	ExceptionPolicy []ExceptionPolicy `json:"exception_policy,omitempty"`
 }
 
 func (f Flow) Empty() bool {
-	return f.PktsToserver == 0 && f.PktsToclient == 0 && f.BytesToserver == 0 && f.BytesToclient == 0 && f.Start == nil && f.End == nil && f.Age == 0 && f.State == "" && f.Reason == ""
+	return f.PktsToserver == 0 && f.PktsToclient == 0 && f.BytesToserver == 0 && f.BytesToclient == 0 &&
+		f.Start == "" && f.End == "" && f.Age == 0 && f.State == "" && f.Reason == ""
 }
 
 type PacketInfo struct {
@@ -51,19 +74,20 @@ type PacketInfo struct {
 // EveEvent is the huge struct which can contain a parsed suricata eve.json
 // log event.
 type EveEvent struct {
-	Timestamp *Time  `json:"timestamp"`
-	EventType string `json:"event_type"`
-	FlowID    int64  `json:"flow_id,omitempty"`
-	InIface   string `json:"in_iface,omitempty"`
-	Vlan      int    `json:"vlan,omitempty"`
-	SrcIP     string `json:"src_ip,omitempty"`
-	SrcPort   int    `json:"src_port,omitempty"`
-	DestIP    string `json:"dest_ip,omitempty"`
-	DestPort  int    `json:"dest_port,omitempty"`
-	Proto     string `json:"proto,omitempty"`
-	AppProto  string `json:"app_proto,omitempty"`
-	TxID      int    `json:"tx_id,omitempty"`
-	TCP       *TCP   `json:"tcp,omitempty"`
+	Timestamp         *Time  `json:"timestamp"`
+	EventType         string `json:"event_type"`
+	FlowID            int64  `json:"flow_id,omitempty"`
+	InIface           string `json:"in_iface,omitempty"`
+	Vlan              int    `json:"vlan,omitempty"`
+	SrcIP             string `json:"src_ip,omitempty"`
+	SrcPort           int    `json:"src_port,omitempty"`
+	DestIP            string `json:"dest_ip,omitempty"`
+	DestPort          int    `json:"dest_port,omitempty"`
+	Proto             string `json:"proto,omitempty"`
+	AppProto          string `json:"app_proto,omitempty"`
+	TxID              int    `json:"tx_id,omitempty"`
+	TCP               *TCP   `json:"tcp,omitempty"`
+	EtherParticipants *Ether `json:"ether,omitempty"`
 
 	PacketDetails *PacketInfo `json:"packet_info,omitempty"`
 
